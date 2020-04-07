5ed59d83b7ab88c8af693e0a1d0f0a17f6fcf8fd
Thomas Sioutas <thsioutas@gmail.com>
2020-04-07 19:25:47 +0200

    Update route_planner methods
        Implement A* search
        Fix g_value calculation
        Improve some methods

--- src/route_planner.cpp
+++ src/route_planner.cpp
@@ -23,23 +23,20 @@ void RoutePlanner::AddNeighbors(RouteModel::Node *current_node) {
         // - For each node in current_node.neighbors, set the parent, the h_value, the g_value. 
         neighbor->parent = current_node;
         neighbor->h_value = CalculateHValue(neighbor);
-        neighbor->g_value = neighbor->distance(*start_node);
+        neighbor->g_value = current_node->g_value + current_node->distance(*neighbor);
         // - For each node in current_node.neighbors, add the neighbor to open_list and set the node's visited attribute to true.
         open_list.push_back(neighbor);
         neighbor->visited = true;
     }
 }
 
-inline bool compareFValue(RouteModel::Node *node1, RouteModel::Node *node2)
-{
-    return ((node1->g_value + node1->h_value) > (node2->g_value + node2->h_value));
-}
-
 RouteModel::Node *RoutePlanner::NextNode() {
     // - Sort the open_list according to the sum of the h value and g value.
-    sort(open_list.begin(), open_list.end(), compareFValue);
+    std::sort(open_list.begin(), open_list.end(), [](const auto & _1st, const auto & _2nd){
+        return _1st->h_value + _1st->g_value > _2nd->h_value + _2nd->g_value;
+    });
     // - Create a pointer to the node in the list with the lowest sum.
-    RouteModel::Node *nextNode = open_list.back();
+    auto nextNode = open_list.back();
     // - Remove that node from the open_list.
     open_list.pop_back();
     // - Return the pointer.
@@ -53,7 +50,7 @@ std::vector<RouteModel::Node> RoutePlanner::ConstructFinalPath(RouteModel::Node 
 
     // - This method should take the current (final) node as an argument and iteratively follow the 
     //   chain of parents of nodes until the starting node is found.
-    while (current_node != start_node) {
+    while ((current_node->x != start_node->x) || (current_node->y != start_node->y)) {
         // - For each node in the chain, add the distance from the node to its parent to the distance variable.
         distance += current_node->distance(*current_node->parent);
         path_found.insert(path_found.begin(), *current_node);
@@ -63,20 +60,22 @@ std::vector<RouteModel::Node> RoutePlanner::ConstructFinalPath(RouteModel::Node 
 
     distance *= m_Model.MetricScale(); // Multiply the distance by the scale of the map to get meters.
     return path_found;
-
 }
 
-
-// TODO 7: Write the A* Search algorithm here.
-// Tips:
-// - Use the AddNeighbors method to add all of the neighbors of the current node to the open_list.
-// - Use the NextNode() method to sort the open_list and return the next node.
-// - When the search has reached the end_node, use the ConstructFinalPath method to return the final path that was found.
-// - Store the final path in the m_Model.path attribute before the method exits. This path will then be displayed on the map tile.
-
 void RoutePlanner::AStarSearch() {
-    RouteModel::Node *current_node = nullptr;
-
-    // TODO: Implement your solution here.
-
+    RouteModel::Node *current_node = start_node;
+    current_node->visited = true;
+    open_list.push_back(current_node);
+    while (open_list.size() > 0) {
+        // - Use the NextNode() method to sort the open_list and return the next node.
+        current_node = NextNode();
+        // - When the search has reached the end_node, use the ConstructFinalPath method to return the final path that was found.
+        if (current_node->distance(*end_node) == 0) {
+            // - Store the final path in the m_Model.path attribute before the method exits. This path will then be displayed on the map tile.
+            m_Model.path = ConstructFinalPath(current_node);
+            return;
+        }
+        // - Use the AddNeighbors method to add all of the neighbors of the current node to the open_list.
+        AddNeighbors(current_node);
+    }
 }
